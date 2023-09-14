0f13926499b9df8ec7767e47d01c823b2218b615
Dhina <dhinakaran.jani@yahoo.com>
2023-09-14 16:03:52 -0400

    initial commit

---
+++ src/main/java/com/abc/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.abc.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@GetMapping("/hello")
+	public String getString() {
+		return "Hello There !!!";
+	}
+
+}
---
+++ src/test/java/com/abc/restaurant/table/reservation/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abc.restaurant.table.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
