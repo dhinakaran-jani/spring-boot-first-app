fb461a29ac75e859730045276e5484bc943d83ae
Dhina <dhinakaran.jani@yahoo.com>
2023-09-24 23:29:38 -0400

    Extend Spring Boot class with SpringBootServletInitializer for Tomcat

--- src/main/java/com/abc/Application.java
+++ src/main/java/com/abc/Application.java
@@ -2,9 +2,11 @@ package com.abc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+//SpringBootServletInitializer is required to deploy as a WAR in Tomcat server
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
